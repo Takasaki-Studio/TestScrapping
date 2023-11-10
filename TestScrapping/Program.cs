49595a1e6f3a8c316545137a2247af0544caaad9
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2023-11-10 08:52:55 -0300

    Add: url by arg

--- TestScrapping/Program.cs
+++ TestScrapping/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net;
 using System.Text;
 
-var url = Environment.GetEnvironmentVariable("TS_URL");
+var url = args.Length > 1 ? args[1] : Environment.GetEnvironmentVariable("TS_URL");
 
 if (string.IsNullOrWhiteSpace(url))
 {
@@ -37,7 +37,8 @@ for (var i = 0; i < 100; i++)
 {
     var request = new HttpRequestMessage(HttpMethod.Get, url);
     request.Headers.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0");
-    request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+    request.Headers.Add("Accept",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
     request.Headers.Add("Accept-Encoding", "gzip, deflate, br");
     request.Headers.Add("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3");
     request.Headers.Add("Alt-Used", uri.Host);
@@ -46,7 +47,7 @@ for (var i = 0; i < 100; i++)
     request.Headers.Add("Sec-Fetch-Mode", "navigate");
     request.Headers.Add("Sec-Fetch-Site", "same-origin");
     request.Headers.Add("Upgrade-Insecure-Requests", "1");
-    
+
     var response = await httpClient.SendAsync(request);
     var responsesBytes = await response.Content.ReadAsByteArrayAsync();
     var responseString = Encoding.UTF8.GetString(responsesBytes);
